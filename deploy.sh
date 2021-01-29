f3e187101fb5c2282eca6fb24448ff4335761372
El-Awny Muhammad <elawnymohamad@gmail.com>
2021-01-29 19:45:38 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:El-AwnyMuhammad/Github_repos (Vue).git master:gh-pages
+git push -f git@github.com:El-AwnyMuhammad/Most-Starred-Repos.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Github_repos (Vue)/'
+    ? '/Most-Starred-Repos/'
     : '/'
 }
